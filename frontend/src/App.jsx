ec8688d6bd91c7dedb1a98e6d3347c4d078dba90
yanzeradev <yankarlos.solraknay@hotmail.com>
2026-05-27 15:10:33 -0300

    adjusting conection front with api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,122 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
+  const [urlOriginal, setUrlOriginal] = useState('');
+  const [urlShort, setUrlShort] = useState('');
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+  const handleShorten = async (e) => {
+    e.preventDefault();
+    //alert(`O usuário quer encurtar: ${urlOriginal}`);
+
+    try {
+      const response = await axios.post('http://localhost:3000/shorten', {
+        urlOriginal: urlOriginal
+      });
+
+      const urlCreate = response.data.urlShort;
+
+      setUrlShort(urlCreate);
+    }catch (error) {
+      console.error("Erro ao comunicar com backend:", error);
+      alert("Erro ao encurtar a URL, verifique se o backend está rodando.")
+    }
+  }
+
+  return(
+    <div className="container">
+      <h1>Encurtador de URL</h1>
+      <p>Cole sua URL para ser encurtada</p>
+
+      <form onSubmit={handleShorten}>
+        <input
+          type="url"
+          placeholder="https://exemplo.com/caminho-longo"
+          required
+          value={urlOriginal}
+          onChange={(e) => setUrlOriginal(e.target.value)}
+        />
+        <button type="submit">Encurtar URL</button>
+      </form>
+
+      {urlShort && (
+        <div className="result">
+          <p>Seu link encurtado está pronto!</p>
+          <a href={urlShort} target="_blank" rel="noreferrer">
+            {urlShort}
+          </a>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+      )}
+    </div>
+  );
 }
-
-export default App
